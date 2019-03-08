02401ce8e0eb8b874872c9d94de598ba1f1b806e
hashelse <hashelse@gmail.com>
2019-03-08 15:31:18 +0530

    added facedetect frontend

---
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import './ImageLinkForm.css';
+
+const ImageLinkForm = ({onInputChange,onButtonSubmit}) => {
+    return (
+        <div className="ma0 mt0">
+            <p className="f3 courier washed-red shadow-2 pa2 ">
+                {'FaceDetect can detect face in pictures'}<br />
+                {'Enter URL of image and hit detect'}
+            </p>
+            <div className="center" style={{clear:'both'}}>
+                <div className="form center pa4 br3 shadow-5">
+                    <input type="text" className="f4 br2 pa2 w-70 center" onChange={onInputChange}/>
+                    <button className="f4 w-30 ml3 grow br2 link ph2 pv2 div white b--black-30
+                    bg-dark-green"
+                    onClick={onButtonSubmit}>Detect</button> 
+                </div>
+
+                
+            </div>
+        </div>
+    )
+}
+
+export default ImageLinkForm;
