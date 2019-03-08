6364671f7c5135c989bc0b5b4f267df0cde30f5f
hashelse <hashelse@gmail.com>
2019-03-08 16:02:44 +0530

    Updates

---
+++ precache-manifest.c3d3cb0cf9e7980db50e75f608970bc3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "862a8f2528d7fb502170",
+    "url": "/facedetect/static/js/runtime~main.862a8f25.js"
+  },
+  {
+    "revision": "b8d85207e6411e171cfb",
+    "url": "/facedetect/static/js/main.b8d85207.chunk.js"
+  },
+  {
+    "revision": "4c84c818e7f00ebfc5d9",
+    "url": "/facedetect/static/js/1.4c84c818.chunk.js"
+  },
+  {
+    "revision": "b8d85207e6411e171cfb",
+    "url": "/facedetect/static/css/main.a758074a.chunk.css"
+  },
+  {
+    "revision": "4c84c818e7f00ebfc5d9",
+    "url": "/facedetect/static/css/1.ddb28503.chunk.css"
+  },
+  {
+    "revision": "995ffcf185d566293aa8cc5523643f17",
+    "url": "/facedetect/index.html"
+  }
+];
